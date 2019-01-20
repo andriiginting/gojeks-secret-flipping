7dd5fc308f4da52033e8fc9daab55e37abd8a9c5
Andri Suranta Ginting <andris.ginting@gmail.com>
2019-01-20 17:45:15 +0700

    [andri] initial commit

---
+++ app/src/main/java/com/example/andriginting/flippers/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.andriginting.flippers
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+    }
+
+}
