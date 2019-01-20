fd46140ab50afa4e6df2fa9268ac68e6304d87b8
Andri Suranta Ginting <andris.ginting@gmail.com>
2019-01-20 18:15:40 +0700

    [andri] add animations func

--- app/src/main/java/com/example/andriginting/flippers/MainActivity.kt
+++ app/src/main/java/com/example/andriginting/flippers/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.andriginting.flippers
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +13,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var animRightOut: AnimatorSet
+    private lateinit var animLeftIn: AnimatorSet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+    }
 
+    private fun changeViewState() {
+        val d = 8000
+        val viewScale = resources.displayMetrics.density.times(d)
+    }
+
+    @SuppressLint("ResourceType")
+    private fun loadAnimations(){
+        animRightOut = AnimatorInflater.loadAnimator(this, R.anim.coin_animator) as AnimatorSet
+        animLeftIn = AnimatorInflater.loadAnimator(this, R.anim.in_coin_animator) as AnimatorSet
     }
 
 }
