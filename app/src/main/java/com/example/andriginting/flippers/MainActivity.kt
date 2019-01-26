214c8086102e12c5a43d2cc02264d0902936b992
Andri Suranta Ginting <andris.ginting@gmail.com>
2019-01-26 12:39:38 +0700

    [andri] finished all the layout component base

--- app/src/main/java/com/example/andriginting/flippers/MainActivity.kt
+++ app/src/main/java/com/example/andriginting/flippers/MainActivity.kt
@@ -4,8 +4,9 @@ import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.util.DisplayMetrics
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -17,29 +18,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var animLeftIn: AnimatorSet
 
     private lateinit var frontCoins: ImageView
-    private lateinit var backCoins: ImageView
     private lateinit var randomPoint: TextView
 
-    private var isBackCoinsVisible: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.navigationIcon = resources.getDrawable(R.drawable.gopoints)
         setSupportActionBar(toolbar)
 
         frontCoins = findViewById(R.id.coins)
         randomPoint = findViewById(R.id.random_point)
-        backCoins = findViewById(R.id.coins)
 
         loadAnimations()
         changeViewState()
+
     }
 
     private fun changeViewState() {
         val spinnerTimes = 8000
         val viewScale = resources.displayMetrics.density.times(spinnerTimes)
         frontCoins.cameraDistance = viewScale
-        backCoins.cameraDistance = viewScale
+
     }
 
     @SuppressLint("ResourceType")
@@ -48,9 +48,4 @@ class MainActivity : AppCompatActivity() {
         animLeftIn = AnimatorInflater.loadAnimator(this, R.anim.in_coin_animator) as AnimatorSet
     }
 
-    private fun rotateCoins(view: View){
-        if (!isBackCoinsVisible){
-
-        }
-    }
 }
--- gesture/src/main/java/come/example/andriiginting/gesture/GestureListener.kt
+++ gesture/src/main/java/come/example/andriiginting/gesture/GestureListener.kt
@@ -1,29 +1,37 @@
 package come.example.andriiginting.gesture
 
 import android.content.Context
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
+import java.lang.Exception
 
 /**
  * Created by andriiginting on 20/01/19.
  */
-class GestureListener(val context: Context, val simpleOnGestureListener: GestureDetector.SimpleOnGestureListener) :
-    GestureDetector.SimpleOnGestureListener() {
+class GestureListener(val context: Context) : GestureDetector.SimpleOnGestureListener() {
 
     companion object {
-        const val SWIPE_LEFT = 0
-        const val SWIPE_RIGHT = 1
+        const val SWIPE_THRESHOLD = 100
+        const val SWIPE_PATH_CONSTANT_VALUE = 200
     }
 
-    val gestureDetector: GestureDetector by lazy {
-        GestureDetector(context, this)
-    }
-
-    fun onTouch(event: MotionEvent){
+    override fun onDown(e: MotionEvent?): Boolean {
+        return super.onDown(e)
     }
 
     override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        val result = false
+
+        try {
+            if (e1?.y!! > SWIPE_PATH_CONSTANT_VALUE) {
+                return result
+            }
+        } catch (e: Exception) {
+            Log.e("swipe-gesture", e.message)
+        }
 
         return super.onFling(e1, e2, velocityX, velocityY)
     }
+
 }
